3cfa3d423e9b1bdeaefa11dad1ed448753db0efd
SHMELOV MAKSYM <92797363+Zannenin@users.noreply.github.com>
2025-11-21 20:03:39 +0200

    Add files via upload

---
+++ src/hoc/withAuth.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+
+const withAuth = (Component) => {
+  return (props) => {
+    const isAuth = localStorage.getItem("auth") === "true";
+    return isAuth ? <Component {...props} /> : <Navigate to="/login" />;
+  };
+};
+
+export default withAuth;
